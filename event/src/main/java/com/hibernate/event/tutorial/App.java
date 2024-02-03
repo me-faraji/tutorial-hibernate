ed4b8a7af2f9e2b106170fe99ee6738d4f457f47
ubuntu <me.faraji67@gmail.com>
2024-02-03 23:37:26 +0330

    start event

---
+++ event/src/main/java/com/hibernate/event/tutorial/App.java
@@ -0,0 +1,16 @@
+package com.hibernate.event.tutorial;
+
+import org.hibernate.EmptyInterceptor;
+import org.hibernate.event.spi.EventType;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
